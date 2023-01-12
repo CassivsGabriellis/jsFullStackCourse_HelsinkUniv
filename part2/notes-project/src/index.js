bd8d9219783bad8a71c1bc852b2c4bcb03f3dd40
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-01-11 22:44:06 -0300

    More adds in the Notes Project

--- part2/notes-project/src/index.js
+++ part2/notes-project/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import axios from "axios";
 
-const promise = axios.get('http://localhost:3001/notes');
-console.log(promise);
+import App from "./App";
 
-const promise2 = axios.get('http://localhost:3001/foobar');
+axios.get("http://localhost:3001/notes").then((response) => {
+  const notes = response.data;
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />);
+});
+
+const promise2 = axios.get("http://localhost:3001/foobar");
 console.log(promise2);
 
 const notes = [
