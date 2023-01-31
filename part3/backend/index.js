f5ed67332e9da22b916efe25ca94561135400e20
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-01-30 23:20:36 -0300

    Adds

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
-
 const cors = require("cors");
-app.use(cors());
 
-app.use(express.static('build'))
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+app.use(express.static("build"));
 
 //array notes
 let notes = [
@@ -25,17 +37,6 @@ let notes = [
     important: true,
   },
 ];
-
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
-app.use(requestLogger);
-
-app.use(express.static('build'))
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -85,9 +86,6 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
 app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
