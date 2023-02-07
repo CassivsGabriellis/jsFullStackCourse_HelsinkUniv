e57aec978d5d88bdc8b985771589b8316b27ff44
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-02-06 23:01:12 -0300

    fixes

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const Note = require("./models/note");
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -43,7 +45,9 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
@@ -88,7 +92,7 @@ app.post("/api/notes", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- part3/backend/mongo.js
+++ part3/backend/mongo.js
@@ -1,37 +1,45 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
-}
+// if (process.argv.length < 3) {
+//   console.log("give password as argument");
+//   process.exit(1);
+// }
 
-const password = process.argv[2];
+// const password = process.argv[2];
 
-const url = `mongodb+srv://cassivsfullstack:<password>@cluster0.2dyrqn1.mongodb.net/appNotes?retryWrites=true&w=majority`;
+// const url = `mongodb+srv://cassivsfullstack:${password}@cluster0.2dyrqn1.mongodb.net/appNotes?retryWrites=true&w=majority`;
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+// mongoose.set("strictQuery", false);
+// mongoose.connect(url);
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-
-const Note = mongoose.model("Note", noteSchema);
-
-const note = new Note({
-  content: "React is satisfying!",
-  important: true,
-});
-
-// note.save().then((result) => {
-//   console.log("note saved!");
-//   mongoose.connection.close();
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,
 // });
 
-Note.find({}).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+// noteSchema.set("toJSON", {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toSting();
+//     delete returnedObject._id;
+//     delete returnedObject.__v;
+//   },
+// });
+
+// const Note = mongoose.model("Note", noteSchema);
+
+// const note = new Note({
+//   content: "React is satisfying!",
+//   important: true,
+// });
+
+// // note.save().then((result) => {
+// //   console.log("note saved!");
+// //   mongoose.connection.close();
+// // });
+
+// Note.find({}).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
