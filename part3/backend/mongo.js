580b7a822215b6db3f6d4e5aac09e4fc10b16095
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-03-04 17:00:21 -0300

    Refactored part3 backend code

--- part3/backend/mongo.js
+++ part3/backend/mongo.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
+  console.log('give password as argument');
   process.exit(1);
 }
 
@@ -9,7 +9,7 @@ const password = process.argv[2];
 
 const url = `mongodb+srv://cassivsfullstack:${password}@cluster0.2dyrqn1.mongodb.net/appNotes?retryWrites=true&w=majority`;
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
@@ -17,7 +17,7 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 });
 
-noteSchema.set("toJSON", {
+noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toSting();
     delete returnedObject._id;
@@ -25,7 +25,7 @@ noteSchema.set("toJSON", {
   },
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model('Note', noteSchema);
 
 // const note = new Note({
 //   content: "React is satisfying!",
